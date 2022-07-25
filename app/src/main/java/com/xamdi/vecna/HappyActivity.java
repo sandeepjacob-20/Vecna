20e879bdf05018103d0ad51ff41568e00863c730
Sandeep Jacob <sandeepjacob1@gmail.com>
2022-07-25 23:40:24 +0530

    Added support in happy emotion and changed app background

--- app/src/main/java/com/xamdi/vecna/HappyActivity.java
+++ app/src/main/java/com/xamdi/vecna/HappyActivity.java
@@ -18,7 +18,7 @@ import com.denzcoskun.imageslider.models.SlideModel;
 import java.util.ArrayList;
 
 public class HappyActivity extends AppCompatActivity {
-    private ImageButton btn;
+    private ImageButton pl1,pl2,pl3;
     private ImageSlider imageSlider;
     private Button moreHelp;
     @Override
@@ -28,9 +28,11 @@ public class HappyActivity extends AppCompatActivity {
         imageSlider = findViewById(R.id.imageslider);
 
         ArrayList<SlideModel> slideModels = new ArrayList<>();
-        slideModels.add(new SlideModel("https://upload.wikimedia.org/wikipedia/en/5/59/The_Gray_Man_poster.png", ScaleTypes.FIT));
-        slideModels.add(new SlideModel("https://m.media-amazon.com/images/M/MV5BNDIzNDU0YzEtYzE5Ni00ZjlkLTk5ZjgtNjM3NWE4YzA3Nzk3XkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_.jpg",ScaleTypes.FIT));
-
+        slideModels.add(new SlideModel("https://m.media-amazon.com/images/M/MV5BN2EwM2I5OWMtMGQyMi00Zjg1LWJkNTctZTdjYTA4OGUwZjMyXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_FMjpg_UX1000_.jpg", ScaleTypes.FIT));
+        slideModels.add(new SlideModel("https://m.media-amazon.com/images/M/MV5BNGQwZjg5YmYtY2VkNC00NzliLTljYTctNzI5NmU3MjE2ODQzXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg",ScaleTypes.FIT));
+        slideModels.add(new SlideModel("https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_FMjpg_UX1000_.jpg",ScaleTypes.FIT));
+        slideModels.add(new SlideModel("https://m.media-amazon.com/images/M/MV5BMjIyNTQ5NjQ1OV5BMl5BanBnXkFtZTcwODg1MDU4OA@@._V1_.jpg",ScaleTypes.FIT));
+        slideModels.add(new SlideModel("https://m.media-amazon.com/images/M/MV5BMTY3NjY0MTQ0Nl5BMl5BanBnXkFtZTcwMzQ2MTc0Mw@@._V1_.jpg",ScaleTypes.FIT));
         imageSlider.setImageList(slideModels, ScaleTypes.FIT);
 
         moreHelp = findViewById(R.id.morehelp);
@@ -40,11 +42,27 @@ public class HappyActivity extends AppCompatActivity {
                 goToUrl("https://buddyhelp.org/");
             }
         });
-        btn = findViewById(R.id.button2);
-        btn.setOnClickListener(new View.OnClickListener() {
+        pl1 = findViewById(R.id.playlist1);
+        pl1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                goToUrl("https://open.spotify.com/track/60nZcImufyMA1MKQY3dcCH?si=30799f0e20ef4b8d");
+                goToUrl("https://open.spotify.com/playlist/2yoO1HV8OgicpB1zsNrvXX?si=1f2607ea59fd471c");
+            }
+        });
+        pl2 = findViewById(R.id.playlist2);
+        pl2.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                goToUrl("https://open.spotify.com/playlist/1cV7hW9JSoNBJOg7ez73cF?si=82c245b00d644318");
+            }
+        });
+        pl3 = findViewById(R.id.playlist3);
+        pl3.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                goToUrl("https://open.spotify.com/playlist/0LJ37WDbjuQRaQ89miPnRw?si=59de505b56554c0c");
             }
         });
     }
